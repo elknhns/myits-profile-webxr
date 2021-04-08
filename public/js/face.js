afe374c1b314362108e716d8d6fb419917bafd4f
elknhns <elknhns@gmail.com>
2021-04-09 02:27:04 +0700

    Minor adjustments

--- public/js/face.js
+++ public/js/face.js
@@ -1,12 +1,8 @@
-console.log('video')
-
 const video = document.getElementById('video')
 
 Promise.all([
-    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-    faceapi.nets.faceExpressionNet.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
     faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
 ]).then(startVideo)
 
@@ -16,35 +12,52 @@ function startVideo() {
         stream => video.srcObject = stream,
         err => console.error(err)
     )
+
+    recognizeFaces()
 }
 
-video.addEventListener('play', () => {
-    const canvas = faceapi.createCanvasFromMedia(video)
-    document.body.append(canvas)
-    const displaySize = { width: video.width, height: video.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    setInterval(async () => {
-        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
-        const resizedDetections = faceapi.resizeResults(detections, displaySize)
-        // canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-        // faceapi.draw.drawDetections(canvas, resizedDetections)
-        // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
-        // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
-        resizedDetections.forEach(detection => {
-            const box = detection.detection.box
-            const drawBox = new faceapi.draw.DrawBox(box, { label: 'Face' })
-            drawBox.draw(canvas)
-        })
-    }, 100)
-})
+async function recognizeFaces() {
+    const labeledDescriptors = await loadLabeledImages()
+    const faceMatcher = new faceapi.FaceMatcher(labeledDescriptors, 0.6)
+    
+    video.addEventListener('play', () => {
+        const canvas = faceapi.createCanvasFromMedia(video)
+        document.body.append(canvas)
+        const displaySize = { width: video.width, height: video.height }
+        faceapi.matchDimensions(canvas, displaySize)
+        setInterval(async () => {
+            const detections = await faceapi.detectAllFaces(video).withFaceLandmarks().withFaceDescriptors()
+            const resizedDetections = faceapi.resizeResults(detections, displaySize)
+            canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+
+            const results = resizedDetections.map((d) => {
+                return faceMatcher.findBestMatch(d.descriptor)
+            })
+            // faceapi.draw.drawDetections(canvas, resizedDetections)
+            // faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+            // faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+            results.forEach(result => {
+                const box = resizedDetections.detection.box
+                const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+                drawBox.draw(canvas)
+            })
+        }, 100)
+    })
+}
 
 function loadLabeledImages() {
     const labels = ['Elkana Hans']
     return Promise.all(
         labels.map(async label => {
-            for (let i = 0; i < 2; i++) {
-                const img
+            const descriptions = []
+            for (let i = 1; i < 2; i++) {
+                const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/elknhns/myits-profile-webxr/master/public/labeled_images/${label}/${i}.jpg`)
+                const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+                descriptions.push(detections.descriptor)
+                // console.log('Add image')
             }
+            console.log(label + "'s face loaded")
+            return new faceapi.LabeledFaceDescriptors(label, descriptions)
         })
     )
 }
